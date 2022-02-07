138f7ae7bbce3fe274b94ee01f4bc2e23d4240e5
javier muñoz <x_jemd_x@hotmail.com>
2022-02-07 20:37:05 -0300

    slides ok en home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,11 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
   }
 ];
 @NgModule({
