9ec6a05ba434c4b5ea5496ed233761ca6adac019
Javier Muñoz <x_jemd_x@hotmail.com>
2022-05-13 15:26:45 +0000

    vista accounts selecciona cuentas

--- src/app/pages/tabs/accounts/accounts.page.ts
+++ src/app/pages/tabs/accounts/accounts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from 'src/app/models/account.model';
 
 @Component({
   selector: 'app-accounts',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsPage implements OnInit {
 
+  accounts: Account[] = [
+    {
+      name: 'Banco de Chile',
+      number: '00 2548 2569',
+      type: 'debit'
+    },
+    {
+      name: 'Banco de Chile',
+      number: '00 2548 2569',
+      type: 'credit'
+    },
+  ]
+
+  accountSelected: Account
+
   constructor() { }
 
   ngOnInit() {
+    this.accountSelected = this.accounts[Math.random().toFixed()]
+  }
+
+  nextAccount() {
+    const selectedAccountId = this.accounts.findIndex(account => account === this.accountSelected);
+    (selectedAccountId === this.accounts.length - 1) ? this.accountSelected = this.accounts[0] : this.accountSelected = this.accounts[selectedAccountId + 1]
+  }
+  
+  prevAccount() {
+    const selectedAccountId = this.accounts.findIndex(account => account === this.accountSelected);
+    (!selectedAccountId) ? this.accountSelected = this.accounts[this.accounts.length - 1] : this.accountSelected = this.accounts[selectedAccountId - 1]
+  }
+
+  addAccount() {
+    
   }
 
 }
